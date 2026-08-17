ef93547676e08e2c321d6179d9e13f457550beab
Roberto Barros <roberto.f.barros@gmail.com>
2026-08-17 04:01:31 -0300

    Ajusta atraso de exibição do Dock

--- install.sh
+++ install.sh
@@ -148,7 +148,7 @@ set_macos_defaults() {
 
   # Dock
   defaults write com.apple.dock autohide-time-modifier -float 0
-  defaults write com.apple.dock autohide-delay -float 0
+  defaults write com.apple.dock autohide-delay -float 0.5
   defaults write com.apple.dock springboard-show-duration -float 0
   defaults write com.apple.dock springboard-hide-duration -float 0
   defaults write com.apple.dock springboard-page-duration -float 0
